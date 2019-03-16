0997398e81ddd791fe67915214369c185c3df262
afrontend <frontend.wordpress@gmail.com>
2019-03-16 23:47:31 +0900

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# fp-block [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> library for fp-block-game
+
+## Installation
+
+```sh
+$ npm install --save fp-block
+```
+
+## Usage
+
+```js
+const fpBlock = require('fp-block');
+
+fpBlock('Rainbow');
+```
+## License
+
+MIT © [Bob Hwang](https://agvim.wordpress.com)
+
+
+[npm-image]: https://badge.fury.io/js/fp-block.svg
+[npm-url]: https://npmjs.org/package/fp-block
+[travis-image]: https://travis-ci.org/afrontend/fp-block.svg?branch=master
+[travis-url]: https://travis-ci.org/afrontend/fp-block
+[daviddm-image]: https://david-dm.org/afrontend/fp-block.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/afrontend/fp-block
+[coveralls-image]: https://coveralls.io/repos/afrontend/fp-block/badge.svg
+[coveralls-url]: https://coveralls.io/r/afrontend/fp-block
---
+++ src/example.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+const clear = require("clear");
+const keypress = require("keypress");
+const program = require("commander");
+const fpBlock = require("../lib/index.js");
+const pkg = require("../package.json");
+
+program
+  .version(pkg.version)
+  .option("-f, --full", "terminal full size")
+  .parse(process.argv);
+
+const startGame = (rows = 15, columns = 15) => {
+  const global = {
+    state: fpBlock.initBlockTable(rows, columns)
+  };
+
+  keypress(process.stdin);
+
+  process.stdin.on("keypress", (ch, key) => {
+    if (key && key.ctrl && key.name === "c") {
+      process.exit();
+    }
+    if (key && key.name === "q") {
+      process.exit();
+    }
+    if (key) {
+      global.state = fpBlock.keyBlockTable(key.name, global.state);
+    }
+  });
+
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+
+  const format = ary => (
+    ary.map(
+      r => (
+        r.map(item => (item.color === "grey"? " " : "■")).join(" ")
+      )
+    )
+  );
+
+  global.timer = setInterval(() => {
+    global.state = fpBlock.moveBlockTable(global.state);
+    clear();
+    console.log(format(fpBlock.joinBlockTable(global.state)));
+  }, 300);
+};
+
+const activate = program => {
+  if (program.full) {
+    startGame(process.stdout.rows - 2, process.stdout.columns / 2 - 4);
+  } else {
+    startGame();
+  }
+};
+
+activate(program);
